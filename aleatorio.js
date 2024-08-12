fb47879685299f6ceb089226e88080964a926eb8
leonardogc <105812641+leonardocamargo10@users.noreply.github.com>
2024-08-12 08:50:59 -0300

    Delete aleatorio.js

--- aleatorio.js
+++
@@ -1,8 +0,0 @@
-const nomes = ["Bruna", "Angelica", "Maria Eduarda", "José", "Amanda", "Lucas", "Gabriel"];
-
-export function aleatorio (lista){
-    const posicao = Math.floor(Math.random()* lista.length);
-    return lista[posicao];
-}
-
-export const nome = aleatorio(nomes)
